ad15c90d6a98c405a66d392c46631a85a3b3f276
Beca <234265987+becsnr@users.noreply.github.com>
2025-11-11 09:11:48 -0300

    while e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INICIO
+/*var c = 1;
+while (c <= 5) { 
+    console.log(`Passo ${c}`);
+    c++;
+} */
+
+// ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL
+
+var c = 1;
+
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 10);
