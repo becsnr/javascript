20e93043bfdab3ff34bb75a8b36e79a409e0049d
Beca <234265987+becsnr@users.noreply.github.com>
2025-11-12 17:25:42 -0300

    acho q agr foi hein

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -4,12 +4,14 @@ function adicionar() {
     let n = document.getElementById('num');
     let lista = document.getElementById('lista');
 
-    if (n.value.length == 0 || n.value < 1 || n.value > 100) {
+    // verifica se o número digitado é válido
+    if (n.value.length == 0 || n.value < 1 || n.value > 100 || numeros.includes(Number(n.value))) {
         alert('Valor inválido ou já adicionado na lista!');
     } else {
         let num = Number(n.value);
-        numeros.push(num) // add o num na lista
-        let item = document.createElement('option')
+        numeros.push(num); // add o num na lista
+
+        let item = document.createElement('option');
         item.text = `Valor ${num} adicionado.`;
         lista.appendChild(item); 
     }
@@ -18,8 +20,30 @@ function adicionar() {
 
 function finalizar() {
     let res = document.getElementById('res');
-    numeros.sort()
+
+    // verifica se digitou algum valor
+    if (numeros.length == 0) {
+        alert('Adicione valores antes de finalizar!');
+        return;
+    }
 
     let total = numeros.length;
-    let maior = numeros
+    /* Os três pontinhos (...) se chamam operador spread (ou espalhamento).
+    Ele serve pra “abrir” uma lista (array) e passar cada valor dela separadamente. */
+    let maior = Math.max(...numeros);
+    let menor = Math.min(...numeros);
+
+    let soma = 0;
+    for (c = 0; c < total; c++) { // vai percorrer tds os elementos do array
+        soma += numeros[c]; // soma = soma + numeros[c]
+    }
+
+    let media = soma / total;
+
+    res.innerHTML = '';
+    res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`;
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
+    res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`;
+    res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`;
 }
