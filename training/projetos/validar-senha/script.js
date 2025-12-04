a6d3b58a740d176cdf0104baebf8936442049e82
Beca <234265987+becsnr@users.noreply.github.com>
2025-12-04 15:41:52 -0300

    regex funcionando

--- training/projetos/validar-senha/script.js
+++ training/projetos/validar-senha/script.js
@@ -1,1 +1,18 @@
-const caixaSenha = document.getElementById();
+const caixaSenha = document.getElementById('senha');
+const validar = document.getElementById('botaoValidar');
+const res = document.getElementById('res');
+
+const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+
+
+validar.addEventListener("click", () => {
+    const senha = caixaSenha.value;
+
+    
+
+    if (regex.test(senha)) {
+        res.innerHTML = "Senha válida!";
+    } else {
+        res.innerHTML = "Senha inválida!";
+    };
+});
