47044449ba75ec0c4721f23cc61a1da0fb7ad3a3
Beca <234265987+becsnr@users.noreply.github.com>
2025-12-10 15:59:58 -0300

    json funcionando

--- training/json/ex001/script.js
+++ training/json/ex001/script.js
@@ -1,3 +1,13 @@
 const botao = document.getElementById('botao');
 const lista = document.getElementById('lista');
 
+botao.addEventListener("click", () => {
+    fetch("produtos.json")
+        .then(r => r.json())
+        .then(dados => {
+            lista.innerHTML = "";
+            dados.forEach(item => {
+                lista.innerHTML += `<p><strong>${item.nome}</strong> - R$ ${item.preco}</p>`;
+            });
+        })
+})
