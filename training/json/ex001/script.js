4742096b66a2622d094ac18cc51c62db72fac39e
Beca <234265987+becsnr@users.noreply.github.com>
2025-12-10 16:15:36 -0300

    total funcionando mas falta arrumar

--- training/json/ex001/script.js
+++ training/json/ex001/script.js
@@ -1,13 +1,19 @@
 const botao = document.getElementById('botao');
 const lista = document.getElementById('lista');
 
+
 botao.addEventListener("click", () => {
     fetch("produtos.json")
         .then(r => r.json())
         .then(dados => {
             lista.innerHTML = "";
+            let soma = 0;
             dados.forEach(item => {
+                
                 lista.innerHTML += `<p><strong>${item.nome}</strong> - R$ ${item.preco}</p>`;
+
+                soma += item.preco;
             });
+            lista.innerHTML += `<p>Total: R$ ${soma}</p>`;
         })
 })
