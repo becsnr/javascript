51cc5834a32da71425a978ead7ac38a2eba14939
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-19 09:02:06 -0300

    salvando item no localStorage

--- training/localStorage/ex005/script.js
+++ training/localStorage/ex005/script.js
@@ -3,8 +3,14 @@ const pesquisarBtn = document.getElementById('pesquisarBtn');
 const historico = document.getElementById('historico');
 const listaHistorico = document.getElementById('listaHistorico');
 
+const lista = JSON.parse(localStorage.getItem("pesquisas")) || [];
+
 pesquisarBtn.addEventListener("click", () => {
     const texto = textoInput.value;
+
+    lista.push(texto);
+    localStorage.setItem("pesquisas", JSON.stringify(lista));
+    
 
     criarHistorico(texto);
 });
