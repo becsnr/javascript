0e24acbb0357d455116b3a0f001b787fdbf76f59
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-21 16:11:03 -0300

    fetch cep

--- training/fetch/ex001/script.js
+++ training/fetch/ex001/script.js
@@ -1,3 +1,16 @@
 const cepInput = document.getElementById('cep');
 const buscarBtn = document.getElementById('buscarBtn');
 const res = document.getElementById('res');
+
+buscarBtn.addEventListener("click", () => {
+    const cep = cepInput.value.trim();
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resp => resp.json())
+        .then(dados => {
+            res.innerHTML = `CEP: ${dados.cep} <br>Cidade: ${dados.localidade} <br>Estado: ${dados.estado}`;
+        })
+        .catch(erro => {
+            console.log("Erro ao buscar CEP!");
+        })
+});
