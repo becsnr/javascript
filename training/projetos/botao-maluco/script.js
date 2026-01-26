ab902ec06a25ee2e364db1c3cd0d56760608dee8
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-26 08:23:13 -0300

    posição e tamanho do botão

--- training/projetos/botao-maluco/script.js
+++ training/projetos/botao-maluco/script.js
@@ -1,6 +1,20 @@
 const botao = document.getElementById('botao');
 const res = document.getElementById('texto');
 
+let modoFuga = false;
+
+botao.style.position = "absolute";
+
+botao.addEventListener("mouseover", () => {
+    if (!modoFuga) return;
+
+    const x = Math.floor(Math.random() * 200) + 300;
+    const y = Math.floor(Math.random() * 200) + 300;
+
+    botao.style.left = x + "px";
+    botao.style.rigth = y + "px";
+});
+
 const textos = [
     "Oi",
     "Para de clicaaaar",
@@ -22,14 +36,21 @@ function caosCores() {
 }
 
 function caosTamanho() {
-    const i = Math.floor(Math.random() * 40);
+    const i = Math.floor(Math.random() * 200) + 100;
+    botao.style.width = i + "px";
+}
+
+function caosMovimento() {
+    modoFuga = true;
 }
 
 function caosTotal() {
-    const sorteio = Math.floor(Math.random() * 2);
+    const sorteio = Math.floor(Math.random() * 4);
 
     if (sorteio === 0) caosMsg();
     if (sorteio === 1) caosCores();
+    if (sorteio === 2) caosTamanho();
+    if (sorteio === 3) caosMovimento();
 }
 
 botao.addEventListener("click", () => {
