45040cea88d37da8412e8444a64aca5698604c0d
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-26 15:14:06 -0300

    add numero
    verifica se é numero, se for aparece na tela

---
+++ training/projetos/calculadora/script.js
@@ -0,0 +1,27 @@
+const teclado = document.querySelector('.teclado');
+const res = document.getElementById('res');
+
+teclado.addEventListener("click", (e) => {
+    // se ñ for botão retorna
+    if (e.target.tagName !== 'BUTTON') return;
+
+    // pega o valor do botão clicado
+    const valor = e.target.innerText;
+
+    if (!isNaN(valor)) {
+        // se for numero
+        addNum(valor);
+    } else {
+        // se for operador
+        tratarOperador(valor);
+    }
+});
+
+function addNum(n) {
+    // apaga o zero e add o num clicado
+    if (res.innerText === "0") {
+        res.innerText = n;
+    } else { // add os outros nums clicados sem apagar
+        res.innerText += n;
+    }
+}
